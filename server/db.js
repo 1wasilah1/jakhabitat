e9b2ca4e40362e3c7ebe29653be14e88fc2a069b
1wasilah1 <m.wasilahhadi@gmail.com>
2025-09-16 14:57:26 +0700

    perubahan port dari 8080 ke 6500

--- server/db.js
+++ server/db.js
@@ -44,7 +44,7 @@ app.post('/api/db/execute', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Database server running on port ${PORT}`);
 });
